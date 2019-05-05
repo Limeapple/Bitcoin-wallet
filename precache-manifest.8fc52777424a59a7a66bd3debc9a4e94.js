704c57d8f81a25fd135f462b3eff141197e567fc
Limeapple <Kevinliu54321@gmail.com>
2019-05-05 17:10:30 -0500

    Updates

---
+++ precache-manifest.8fc52777424a59a7a66bd3debc9a4e94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18080b8f00eb5643114a",
+    "url": "/Blockchain/static/css/main.6c081820.chunk.css"
+  },
+  {
+    "revision": "18080b8f00eb5643114a",
+    "url": "/Blockchain/static/js/main.be22b3b8.chunk.js"
+  },
+  {
+    "revision": "c5a6d9cb0ba11e8f9135",
+    "url": "/Blockchain/static/js/runtime~main.da55e216.js"
+  },
+  {
+    "revision": "938c97bac77656b38665",
+    "url": "/Blockchain/static/js/2.0381fd13.chunk.js"
+  },
+  {
+    "revision": "51fa3f40605c5f9e10ab217a1eff4102",
+    "url": "/Blockchain/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Clock/precache-manifest.a7148e985dc9231e3421a8bf5e23c6ae.js"
+  "/Blockchain/precache-manifest.8fc52777424a59a7a66bd3debc9a4e94.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Clock/index.html", {
+workbox.routing.registerNavigationRoute("/Blockchain/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
